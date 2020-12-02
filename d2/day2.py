6988bea394f232ff00fb1acaa938478c7806f16b
rpm4real <rpm4real@gmail.com>
2020-12-02 15:18:34 +0000

    adding more day2 code, which solves d2 puzzles and adds some comments

--- d2/day2.py
+++ d2/day2.py
@@ -5,19 +5,25 @@ with open('d2/input.txt') as file:
 pass_poli_lookup = [k.split(': ') for k in pass_policies]
 
 #%%
-
+# count cases where passwords contain character x times
 success = 0 
 for poli, password in pass_poli_lookup: 
     occur, letter = poli.split(' ')
     actual_occur = password.count(letter)
     min_occur, max_occur = occur.split('-')
-    if occur >= min_occur and occur <= max_occur: 
+    if actual_occur >= int(min_occur) and actual_occur <= int(max_occur): 
         success += 1
-    print(occur, letter)
-    print(min_occur, max_occur)
-    print(actual_occur)
-    if success > 100: 
-        break 
+
 print(success)
 
+# %%
+# look for exactly one of the characters in string locations
+success = 0 
+for poli, password in pass_poli_lookup: 
+    occur, letter = poli.split(' ')
+    occurs = occur.split('-')
+    checks = [password[int(spot)-1] == letter for spot in occurs]
+    if any(checks) and not all(checks): 
+        success += 1
+print(success)
 # %%
