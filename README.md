2a64956443a888e4d6ad75e9dcf4fbf32b1cf5e0
rpm4real <rpm4real@gmail.com>
2020-12-02 02:35:25 +0000

    Small readme changes and formatting changes for d1 code

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 [![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/rpm4real/adventofcode-2020)
 
 # adventofcode-2020
+Assorted python code for solving 2020 Advent of code problems: https://adventofcode.com/2020 
--- d1/day1.py
+++ d1/day1.py
@@ -1,15 +1,18 @@
-with open('input.txt') as file: 
+#%%
+with open('d1/input.txt') as file: 
     nums = file.read().split('\n')
 
 nums = list(map(int, nums))
 
 # get two numbers that sum to 2020 in input.txt 
+#%% 
 for j in range(1010): 
     k = 2020-j 
     if k in nums and j in nums: 
         print(j,k,j*k)
         break
 
+#%% 
 # get three numbers that sum to 2020 
 from itertools import combinations
 from functools import reduce
@@ -19,3 +22,4 @@ for comb in my_combs:
     if sum(comb) == 2020: 
         print(comb, reduce(lambda x,y: x*y,comb))
         break
+
