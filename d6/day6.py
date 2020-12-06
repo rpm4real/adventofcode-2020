d98b8a461419c1c9066b08428c7b7d32757d9bfa
rpm4real <rpm4real@gmail.com>
2020-12-06 11:55:14 -0700

    adding code for part 2 of day 6

--- d6/day6.py
+++ d6/day6.py
@@ -6,11 +6,17 @@ with open('input.txt') as file:
 
 group_df = pd.DataFrame(group_list, columns=['group','answers']).explode('answers')
 # %%
-
+#part 1 
 group_gp = group_df.groupby('group')[['answers']].sum()
 group_gp['nunique'] = group_gp['answers'].apply(set).apply(len)
 
 group_gp['nunique'].sum()
 
 
+# %%
+#part 2 
+group_df['answer_set'] = group_df['answers'].apply(set)
+group_gp_2 = group_df.groupby('group')['answer_set'].apply(lambda x: set.intersection(*x))
+
+group_gp_2.apply(len).sum()
 # %%
