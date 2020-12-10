da08a0413fd3b74acafcde4c52b4c6598d6ccc84
rpm4real <rpm4real@gmail.com>
2020-12-09 21:37:59 -0700

    adding part 2 of d9

--- d9/d9.py
+++ d9/d9.py
@@ -3,8 +3,7 @@ with open('input.txt') as file:
     code = [int(x) for x in file.read().split('\n')]
 
 #%% 
-
-
+# part 1 
 look_back = 25 
 for k, val in enumerate(code[look_back:], start = look_back):
     look_back_range = code[k - look_back:k]
@@ -18,4 +17,22 @@ for k, val in enumerate(code[look_back:], start = look_back):
         break 
 
 print(val, k)
+# %%
+# part 2 
+target = val 
+found = False 
+start = 0 
+end = 1 
+while found is False: 
+    check_range = code[start:end]
+    if sum(check_range) < target: 
+        end += 1 
+    elif sum(check_range) > target: 
+        start += 1
+    else: 
+        found = True 
+
+print(min(check_range), max(check_range))
+# %%
+print(min(check_range)+max(check_range))
 # %%
