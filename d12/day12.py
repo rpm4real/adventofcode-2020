48bf5abe3cf4fa350f6f909ecc91f61b6d429fe8
rpm4real <rpm4real@gmail.com>
2020-12-12 18:22:55 -0700

    Part 2 of day 12 done

--- d12/day12.py
+++ d12/day12.py
@@ -6,11 +6,12 @@ with open('input.txt') as file:
 
 #%% 
 
-def rotation(numb, degree): 
-    if degree == 180 or degree == -180: 
-        return numb*-1 
-    elif degree == 90 
-        return complex(numb.imag, numb.real)
+def rotation(change_dir, prev_orien): 
+    rot = {'L': 1, 'R': -1}[change_dir[0]]
+    new_orien = prev_orien*e**(1j*change_dir[1]*rot*pi/180)
+    return complex(round(new_orien.real), round(new_orien.imag))
+
+#%% 
 
 coords = complex(0,0)
 legend = {
@@ -29,7 +30,20 @@ for dir in directions:
     elif dir[0] in ('L','R'):
         orientation = rotation(dir,orientation)
     
-
+print(coords, abs(coords.real) + abs(coords.imag))
 
 
 # %%
+waypoint_loc = complex(10,1)
+ship_loc = complex(0,0)
+
+for dir in directions: 
+    if dir[0] in legend.keys(): 
+        waypoint_loc += dir[1]*legend[dir[0]]
+    elif dir[0] == 'F': 
+        ship_loc = ship_loc + dir[1]*waypoint_loc
+    elif dir[0] in ('L','R'):
+        waypoint_loc = rotation(dir,waypoint_loc)
+
+print(ship_loc, abs(ship_loc.real) + abs(ship_loc.imag))
+
