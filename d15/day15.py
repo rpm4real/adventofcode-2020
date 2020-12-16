086bda24b38e22ff8b2997a67878a4773abc9737
rpm4real <rpm4real@gmail.com>
2020-12-15 20:20:06 -0700

    Finished part 1 of day 15

---
+++ d15/day15.py
@@ -0,0 +1,19 @@
+#%% 
+input = [1,12,0,20,8,16]
+number_tracker = {}
+turn_cnt = 1 
+for num in input: 
+    number_tracker[num] = turn_cnt 
+    turn_cnt += 1
+    prev_num = num  
+
+while turn_cnt <= 2020: 
+    if prev_num in number_tracker: 
+        new_num = turn_cnt - 1 - number_tracker[prev_num] 
+    else: 
+        new_num = 0
+    number_tracker[prev_num] = turn_cnt-1 #set previous number
+    turn_cnt +=1 
+    prev_num = new_num 
+
+print(prev_num)
