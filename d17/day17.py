bdab4543d41715576da8df95f72cdd652b18d1b1
rpm4real <rpm4real@gmail.com>
2020-12-22 15:06:56 -0700

    Finished part 2 of day 17

--- d17/day17.py
+++ d17/day17.py
@@ -35,3 +35,27 @@ def sim_cycle(state, N):
 active_states = sim_cycle(initial_state, 6)
 len(active_states)
 # %%
+# part 2 
+neighborhood = [c for c in list(product([-1,0,1], repeat = 4)) if c != (0,0,0,0)]
+
+#could define this better 
+initial_state = set([k + (0,) for k in cube_array.keys() if cube_array[k]=='#'])
+
+def shift2(cells, delta):
+    "shift cells by a delta"
+    (dx, dy, dz, dw) = delta
+    return {(x+dx, y+dy, z+dz, w+dw) for (x, y, z, w) in cells}
+
+def sim_cycle2(state, N):
+    "iterate cycle N times"
+    for g in range(N):
+        counts = Counter(n for c in state for n in shift2(neighborhood, c))
+        state = {c for c in counts 
+                if counts[c] == 3 or (counts[c] == 2 and c in state)}
+    return state 
+
+
+# %%
+active_states = sim_cycle2(initial_state, 6)
+len(active_states)
+# %%
