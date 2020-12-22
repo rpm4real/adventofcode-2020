011b910b8d4a8ed9fd91717e1e17a56cd026d368
rpm4real <rpm4real@gmail.com>
2020-12-22 15:00:05 -0700

    Finished day 17 part 1

--- d17/day17.py
+++ d17/day17.py
@@ -1,5 +1,5 @@
 #%%
-with open('input_debug.txt') as file: 
+with open('input.txt') as file: 
     initial_states = file.read().split('\n') 
 
 cube_array = {}
@@ -8,72 +8,30 @@ for row,line in enumerate(initial_states):
         #cube_array.append(CubeState(col, row, 0, state))
         cube_array[(col, row, 0)] = state 
 
-
 # %%
+from collections import Counter 
 from itertools import product
 
-def coord_add(coord1, coord2): 
-    return tuple((x + y for x,y in zip(coord1, coord2)))
+neighborhood = [c for c in list(product([-1,0,1], repeat = 3)) if c != (0,0,0)]
 
-def check_adjacents(coordinate, my_array): 
-    """return number of active adjacent coordinates"""
-    neighborhood = [-1,0,1]
-    search_look = product(neighborhood, repeat = 3)
-    adj_values = 0
-    search_space = [coord_add(coordinate,d) for d in search_look if d != (0,0,0)]
-    f = lambda x: 1 if x == '#' else 0 
-    return sum([f(my_array.get(coord,'.')) for coord in search_space])
+#could define this better 
+initial_state = set([k for k in cube_array.keys() if cube_array[k]=='#'])
 
-def fill_array(my_array): 
+def shift(cells, delta):
+    "shift cells by a delta"
+    (dx, dy, dz) = delta
+    return {(x+dx, y+dy, z+dz) for (x, y, z) in cells}
 
-    from functools import reduce 
-    mins = reduce(lambda x,y: [min(x[k], y[k]) for k in range(3)], cube_array)
-    maxs = reduce(lambda x,y: [max(x[k], y[k]) for k in range(3)], cube_array)
-    for k, layers in enumerate(zip(mins, maxs)): 
-        coord_add( , (1,0,0) )
-    
-    for p in product([0,1],repeat = 3): 
-        my_array[coord_add(mins,(-x for x in p))] = '.'
-        my_array[coord_add(maxs, p)] = '.'
-    return my_array
-
-def sim_cycle(my_array):
-    #new_array = fill_array(my_array)
-    check_coords = [k for k in my_array.items()]
-    #for loc,state in check_coords: 
-    while COND: 
-        num_active = check_adjacents(loc, my_array)
-        if state == '#':  
-            if num_active in (2,3):
-                new_state = '#'
-            else: 
-                new_state = '.'
-        
-        elif state == '.' and num_active == 3:  
-            new_state = '#'
-        else: 
-            new_state = '.'
-        new_array[loc] = new_state
-        #new_array = {**new_array, **fill_cube_array}
-    return new_array
-
-#%% 
-for k in range(2): 
-    cube_array = sim_cycle(cube_array)
-    print(list(cube_array.values()).count('#'))
-
-# %%
-list(cube_array.values()).count('#')
+def sim_cycle(state, N):
+    "iterate cycle N times"
+    for g in range(N):
+        counts = Counter(n for c in state for n in shift(neighborhood, c))
+        state = {c for c in counts 
+                if counts[c] == 3 or (counts[c] == 2 and c in state)}
+    return state 
 
 
 # %%
-##########
-neighborhood = [-1,0,1]
-check_coords = [k for k in cube_array.items()]
+active_states = sim_cycle(initial_state, 6)
+len(active_states)
 # %%
-def print_cube(my_array): 
-    zrange = [coord[2] for coord in my_array.keys()]
-    min_z, max_z = min(zrange), max(zrange)
-    for z in range(min_z, max_z+1): 
-        print('z={0}'.format(z))
-        print_range = [coord for coord in my_array.keys() if coord[2] == z]
