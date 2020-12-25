6a38452939da258fede5dbd98f45ed70e608442d
rpm4real <rpm4real@gmail.com>
2020-12-25 12:56:16 -0700

    Finished part 2 of day 4

--- d4/day4.py
+++ d4/day4.py
@@ -5,9 +5,6 @@ with open('input.txt') as file:
 
 parse1 = [re.split('\s', p) for p in pass_file]
 
-
-
-
 #%%
 pass_list = []
 for passport in parse1: 
@@ -27,28 +24,33 @@ for passport in pass_list:
         pass_cnt +=1 
 
 print(pass_cnt)
+
+#%% 
+import pandas as pd 
+
 # %%
+pass_df = pd.DataFrame(pass_list)
 
-# def hgt(x): 
-#     a = re.findall('cm|in',x)[0]
-#     b = re.split(a, x)[0]
-#     lookup = {
-#         'cm': lambda x: x in range(150,194),
-#         'in': lambda x: x in range(59,77)
-#     }
-#     return lookup.get(a,False)(int(b))
+#%% 
+def check_hgt(row):
+    if 'cm' in row: 
+        val = int(row.split('cm')[0])
+        return (val >= 150) and (val <= 193)
+    elif 'in' in row: 
+        val = int(row.split('in')[0])
+        return (val >= 59) and (val <= 76)
+    else: 
+        return False 
 
-# # %% 
-# 'byr': lambda x: int(x) in range(1920,2003), 
-# 'iyr': lambda x: int(x) in range(2010,2021), 
-# 'eyr': lambda x: int(x) in range(2020,2031), 
-# 'hgt': hgt, 
-# 'hcl': 
+birth_rule = (pd.to_numeric(pass_df['byr']) >= 1920) & (pd.to_numeric(pass_df['byr'])  <= 2002)
+issue_rule = (pd.to_numeric(pass_df['iyr'])  >= 2010) & (pd.to_numeric(pass_df['iyr'])  <= 2020)
+exp_rule = (pd.to_numeric(pass_df['eyr'])  >= 2020) & (pd.to_numeric(pass_df['eyr'])  <= 2030)
+hgt_rule = pass_df['hgt'].apply(str).apply(check_hgt)
+hcl_rule = pass_df['hcl'].str.fullmatch(pat = '#([a-f]|[0-9])*')
+eye_rule = pass_df['ecl'].isin(['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])
+pid_rule = (pass_df['pid'].str.len() == 9) & (pass_df['pid'].str.isnumeric())
 
-# hgt (Height) - a number followed by either cm or in:
-# If cm, the number must be at least 150 and at most 193.
-# If in, the number must be at least 59 and at most 76.
-# hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-# ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-# pid (Passport ID) - a nine-digit number, including leading zeroes.
-# cid (Country ID) - ignored, missing or not.
+
+# %%
+pass_df[birth_rule & issue_rule & exp_rule & hgt_rule & hcl_rule & eye_rule & pid_rule ]
+# %%
